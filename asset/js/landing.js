67f6552792ae570a1781b320eda0dbe686d9eb9f
Abhishekbishtgit <106829025+Abhishekbishtgit@users.noreply.github.com>
2022-07-11 17:22:37 +0530

    Update landing.js

--- asset/js/landing.js
+++ asset/js/landing.js
@@ -11,3 +11,101 @@
     
     $(window).scrollTop(0);
     });
+
+
+ 
+
+    function mybtn1() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more1");
+        var btnText = document.getElementById("myBtn1");
+      
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          btnText.innerHTML = "more"; 
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          btnText.innerHTML = ""; 
+          moreText.style.display = "inline";
+        }
+      }
+
+      function mybtn2() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more2");
+        var btnText = document.getElementById("myBtn2");
+      
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          btnText.innerHTML = "more"; 
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          btnText.innerHTML = ""; 
+          moreText.style.display = "inline";
+        }
+      }
+      function mybtn3() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more3");
+        var btnText = document.getElementById("myBtn3");
+      
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          btnText.innerHTML = "more"; 
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          btnText.innerHTML = ""; 
+          moreText.style.display = "inline";
+        }
+      }
+
+
+      function mybtn4() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more4");
+        var btnText = document.getElementById("myBtn4");
+      
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          btnText.innerHTML = "more"; 
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          btnText.innerHTML = ""; 
+          moreText.style.display = "inline";
+        }
+      }
+   
+      function mybtn6() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more6");
+        var btnText = document.getElementById("myBtn6");
+      
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          btnText.innerHTML = "more"; 
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          btnText.innerHTML = ""; 
+          moreText.style.display = "inline";
+        }
+      }
+      function mybtn7() {
+        var dots = document.getElementById("dots");
+        var moreText = document.getElementById("more7");
+        var btnText = document.getElementById("myBtn7");
+      
+        if (dots.style.display === "none") {
+          dots.style.display = "inline";
+          btnText.innerHTML = "more"; 
+          moreText.style.display = "none";
+        } else {
+          dots.style.display = "none";
+          btnText.innerHTML = ""; 
+          moreText.style.display = "inline";
+        }
+      }
